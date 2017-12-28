f92958c10bac67765aa68ab2e38dc66da7e44ad8
dnguyenlearning <dnguyenlearning@gmail.com>
2017-12-29 01:31:35 +0700

    last try

--- config/config.js
+++ config/config.js
@@ -1,10 +1,9 @@
 module.exports={
     port: process.env.PORT || 3000,
-    Host:  'http://localhost:3000',
+    Host:  'https://projectgreenfood.herokuapp.com' || 'http://localhost:3000',
     database:{
         path:'mongodb://project2t:project2t@ds059207.mlab.com:59207/project2t'
     },
     secret:'thisisafuckingshitsecret,canyougetthis?icant21412jfsalkfj124120i1okthankyou',
 }
 
-//'https://projectgreenfood.herokuapp.com' ||
