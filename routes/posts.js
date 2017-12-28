81c5edf4293c3bba168e46665940973edf5ce7f0
dnguyenlearning <dnguyenlearning@gmail.com>
2017-12-28 18:15:05 +0700

    make it better

--- routes/posts.js
+++ routes/posts.js
@@ -9,7 +9,7 @@ route.post('/upload')
 
 const storage=multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './uploads/');
+        cb(null, path.join(__dirname+ './uploads/'));
     },
     filename:(req,file,cb)=>{
         file.originalname=file.originalname.replace(' ','_');
