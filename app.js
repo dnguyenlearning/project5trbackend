4234fb9290b59cdc1c322561c705863df50ddd4b
dnguyenlearning <dnguyenlearning@gmail.com>
2017-12-28 23:41:27 +0700

    make it better

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ mongoose.connect(config.database.path,(err)=>{
 });
 
 
-// app.use(function(req, res, next) { //allow cross origin requests
-//     res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
-//     res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//     res.header("Access-Control-Allow-Credentials", true);
-//     next();
-// });
+app.use(function(req, res, next) { //allow cross origin requests
+    res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
 
 app.use(express.static(path.join(__dirname,'src')));
 
